24b785a6decdc3ac05b48cf0994698bba37424bd
Fathhaq12 <162147395+Fathhaq12@users.noreply.github.com>
2025-04-20 14:36:51 +0700

    Update NoteList.js

--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import {BASE_URL} from "../utils"
 
 const NoteList = () => {
   const [notes, setNotes] = useState([]);
