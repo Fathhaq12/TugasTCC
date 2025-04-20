3fd45e0e90dacf388b3a3a6d123648d483b3dad5
Fathhaq12 <162147395+Fathhaq12@users.noreply.github.com>
2025-04-20 14:51:08 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,16 +3,6 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('tugasserra', 'root', '', {
     host: '34.27.28.164',
     dialect: 'mysql',
-    logging: false
 });
-
-(async () => {
-    try {
-        await db.authenticate();  
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-})();
 
 export default db;
