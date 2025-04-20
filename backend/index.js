f34df5778794384d018ee2eb0b4e0001de6a908b
serranomantara <162292322+serranomantara@users.noreply.github.com>
2025-04-20 13:26:02 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ import cors from "cors";
 import UserRoute  from "./routes/noteRoute.js";
 
 const app = express();
+const port = process.env.PORT | 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, () => console.log("server up and running..."));
+app.listen(port, () => console.log("server up and running..."));
